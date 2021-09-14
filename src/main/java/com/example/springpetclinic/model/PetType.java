e5f931af54e82249fbf73774c7c3ad798d629bcf
tevrat <tevrataksoy@gmail.com>
2021-09-14 13:37:50 +0300

    Adding classed

---
+++ src/main/java/com/example/springpetclinic/model/PetType.java
@@ -0,0 +1,16 @@
+package com.example.springpetclinic.model;
+
+import javax.servlet.SessionTrackingMode;
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/example/springpetclinic/model/Vet.java
@@ -0,0 +1,10 @@
+package com.example.springpetclinic.model;
+
+
+
+public class Vet extends Person{
+
+
+
+
+}
